248197998e690be8984d10a526d5f33b41354143
Glitch (incredible-lake) <none>
2018-03-05 21:40:32 +0000

    basic outline maybe done?

--- server.js
+++ server.js
@@ -64,9 +64,21 @@ const USERS = [
 //  4. if matching user found, add the user object to the request object
 //     (aka, `req.user = matchedUser`)
 function gateKeeper(req, res, next) {
-  // your code should replace the line below
+  const userInfo = req.get('x-username-and-password');
+  const userInfoParsed = queryString.parse(userInfo);
+  
+  const userMatch = USERS.find(function(user) {
+    return user.userName === userInfoParsed.userName && user.password === userInfoParsed.password;
+  });
+  
+  if (userMatch) {
+    //req.user = THAT_USER;
+  }
+  
   next();
 }
+
+app.use(gateKeeper);
 
 // Add the middleware to your app!
 
